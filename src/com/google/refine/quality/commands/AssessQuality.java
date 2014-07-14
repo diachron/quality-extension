10ebc51947a413297124219c909213e52e0e5221
Muhammad Ali Qasmi <muhammadaliqasmi@gmail.com>
2014-07-14 13:13:38 +0200

    problmetic quad writing done

--- src/com/google/refine/quality/commands/AssessQuality.java
+++ src/com/google/refine/quality/commands/AssessQuality.java
@@ -2,30 +2,106 @@ package com.google.refine.quality.commands;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Serializable;
 import java.util.List;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
+import org.json.JSONWriter;
 
 import com.google.refine.browsing.Engine;
 import com.google.refine.commands.Command;
+import com.google.refine.history.HistoryEntry;
 import com.google.refine.model.Project;
 import com.google.refine.quality.utilities.LoadJenaModel;
 import com.google.refine.quality.utilities.Utilities;
+import com.google.refine.quality.commands.TransformData.EditOneCellProcess;
 import com.google.refine.quality.metrics.AbstractQualityMetrics;
 import com.google.refine.quality.metrics.EmptyAnnotationValue;
+import com.google.refine.quality.metrics.HelloWorldMetrics;
 import com.google.refine.quality.metrics.IncompatibleDatatypeRange;
 import com.google.refine.quality.metrics.MalformedDatatypeLiterals;
 import com.google.refine.quality.metrics.MisplacedClassesOrProperties;
 import com.google.refine.quality.metrics.MisusedOwlDatatypeOrObjectProperties;
 import com.google.refine.quality.metrics.OntologyHijacking;
 import com.google.refine.quality.metrics.WhitespaceInAnnotation;
+import com.google.refine.util.ParsingUtilities;
+import com.google.refine.util.Pool;
 import com.hp.hpl.jena.sparql.core.Quad;
 
 public class AssessQuality extends Command{
+    
+    protected void writeProblemicQuads(HttpServletRequest request, HttpServletResponse response, List<Quad> listQuad) throws ServletException {
+        try {
+            String splitter = "|";
+            /** Get Projet Details **/
+            
+            // Retrieve project object
+            Project project = getProject(request);
+            int i = 0;
+
+            EditOneCellProcess process = null;
+            HistoryEntry historyEntry = null;
+
+            for (Quad tmpQuad : listQuad) {
+
+                int rowIndex = i;
+                int cellIndex = 1;
+
+                String type = "String";
+                String valueString = tmpQuad.getSubject() + splitter + tmpQuad.getPredicate() + splitter
+                        + tmpQuad.getObject();
+                Serializable value = null;
+
+                if ("number".equals(type)) {
+                    value = Double.parseDouble(valueString);
+                } else if ("boolean".equals(type)) {
+                    value = "true".equalsIgnoreCase(valueString);
+                } else if ("date".equals(type)) {
+                    value = ParsingUtilities.stringToDate(valueString);
+                } else {
+                    value = valueString;
+                }
+
+                process = new EditOneCellProcess(project, "Edit single cell", rowIndex, cellIndex, value);
+
+                historyEntry = project.processManager.queueProcess(process);
+                i++;
+            }
+
+            if (historyEntry != null) {
+                /*
+                 * If the operation has been done, return the new cell's data so
+                 * the client side can update the cell's rendering right away.
+                 */
+                JSONWriter writer = new JSONWriter(response.getWriter());
+
+                Pool pool = new Pool();
+                Properties options = new Properties();
+                options.put("pool", pool);
+
+                writer.object();
+                writer.key("code");
+                writer.value("ok");
+                writer.key("historyEntry");
+                historyEntry.write(writer, options);
+                writer.key("cell");
+                process.newCell.write(writer, options);
+                writer.key("pool");
+                pool.write(writer, options);
+                writer.endObject();
+            } else {
+                respond(response, "{ \"code\" : \"pending\" }");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     
     /**
      * Retrieves rdf - turtel data from project. RDF data is only stored in first column of the data grid.
@@ -66,8 +142,9 @@ public class AssessQuality extends Command{
      * 
      * @param abstractQualityMetrics
      * @param listQuad
+     * @throws ServletException 
      */
-    protected void processMetric(AbstractQualityMetrics abstractQualityMetrics, List<Quad> listQuad){
+    protected void processMetric(HttpServletRequest request, HttpServletResponse response, AbstractQualityMetrics abstractQualityMetrics, List<Quad> listQuad) throws ServletException{
         
         System.out.println("Processing for " +  abstractQualityMetrics.getClass());
         
@@ -76,9 +153,11 @@ public class AssessQuality extends Command{
             System.out.println("No problem found for " + abstractQualityMetrics.getClass());
         }
         else {
+            /*
             for (Quad quad : abstractQualityMetrics.getQualityProblems()){
                 Utilities.printQuad(quad, System.out);
-            }
+            }*/
+            writeProblemicQuads(request, response, abstractQualityMetrics.getQualityProblems());
         }
     }
     
@@ -95,18 +174,20 @@ public class AssessQuality extends Command{
             /** For Debug Only **/
             
             //List all Statemtents loaded in model 
-            Utilities.printStatements(LoadJenaModel.getModel(retrieveRDFData(project)).listStatements(), System.out);
+            //Utilities.printStatements(LoadJenaModel.getModel(retrieveRDFData(project)).listStatements(), System.out);
             
             /** Get Project Data **/
-            /******************************************************
+            
             // Retrieve rdf data from project
             InputStream inputStream = retrieveRDFData(project);
             // Retrieve all quad in model
             List<Quad> listQuad = LoadJenaModel.getQuads(inputStream);
             // Close input stream
             inputStream.close();
-            /******************************************************
+            
+            
             /** Compute Metrics **/
+            processMetric(request,response,new HelloWorldMetrics(), listQuad);
             /******************************************************
             // for Empty Annotation value
             EmptyAnnotationValue.loadAnnotationPropertiesSet(null); // Pre-Process
---
+++ src/com/google/refine/quality/metrics/HelloWorldMetrics.java
@@ -0,0 +1,60 @@
+package com.google.refine.quality.metrics;
+
+import org.apache.log4j.Logger;
+
+import com.hp.hpl.jena.sparql.core.Quad;
+
+public class HelloWorldMetrics extends AbstractQualityMetrics{
+    /**
+     * logger static object
+     */
+    private static Logger logger = Logger
+                    .getLogger(HelloWorldMetrics.class);
+    /**
+     * total number of literals
+     */
+    private double totalLiterals = 0;
+    /**
+     * total number of malformed literals
+     */
+    private double helloWorldLiterals = 0;
+    /**
+     * This method does nothing. Its a test metrics
+     * 
+     * @param quad
+     *            - to be identified
+     */
+    @Override
+    public void compute(Quad quad) {
+    
+            logger.trace("compute() --Started--");
+            this.totalLiterals++;
+            this.helloWorldLiterals++;
+            this.problemList.add(quad);
+            logger.trace("compute() --Ended--");
+    }
+
+    /**
+     * Returns metric value for object this class
+     * 
+     * @return (number of hello world literals) / (total number of literals)
+     */
+    @Override
+    public double metricValue() {
+
+            logger.trace("metricValue() --Started--");
+            logger.debug("Hello World Literals :: " + this.helloWorldLiterals);
+            logger.debug("Total Literals :: " + this.totalLiterals);
+
+            // return ZERO if total number of RDF literals are ZERO [WARN]
+            if (0 >= this.totalLiterals) {
+                    logger.warn("Total number of RDF data type literals in given document is found to be zero.");
+                    return 0.0;
+            }
+
+            double metricValue = this.helloWorldLiterals / this.totalLiterals;
+            logger.debug("Metric Value :: " + metricValue);
+            logger.trace("metricValue() --Ended--");
+            return metricValue;
+    }
+}
