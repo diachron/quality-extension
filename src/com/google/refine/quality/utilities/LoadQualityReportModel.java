63541440346374a5b23b4768671b84a5a6771204
Ruslan Kovalov <ruslan.kovalyov@gmail.com>
2014-09-29 15:41:34 +0200

    Fix bug with loading of qprob.rdf.

--- src/com/google/refine/quality/utilities/LoadQualityReportModel.java
+++ src/com/google/refine/quality/utilities/LoadQualityReportModel.java
@@ -1,60 +1,65 @@
 package com.google.refine.quality.utilities;
 
+import java.io.File;
+
+import org.apache.log4j.Logger;
+
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.util.FileManager;
 
-/**
- * Retrieve problems messages from file
- * @author Muhammad Ali Qasmi
- *
- */
 public class LoadQualityReportModel {
-    /**
-     * Default file path name
-     */
-    protected static String filePathName = "extensions/quality-extension/resources/vocabularies/qprob/qprob.rdf";
-    protected static Model qualityReportModel = null;
-    public static String getResourcePropertyValue(Resource resource ,Property property) {
-        
-        if (resource == null || property == null) {
-            return "";
-        }
-        
-        if (LoadQualityReportModel.qualityReportModel == null) {
-            LoadQualityReportModel.qualityReportModel = ModelFactory.createDefaultModel();
-            LoadQualityReportModel.qualityReportModel.read(filePathName);
-            /* -- For Debug Only
-            StmtIterator iter = LoadQualityReportModel.qualityReportModel.listStatements();
-            while(iter.hasNext()){
-                Statement s = iter.nextStatement();
-                System.out.print("Subject :: " + s.getSubject());
-                System.out.print("  --  Predicate :: " + s.getPredicate());
-                System.out.println("  --> Object :: " + s.getObject());
-            }*/
-        }
-        if (LoadQualityReportModel.qualityReportModel.getProperty(resource, property) != null)
-        {
-            return LoadQualityReportModel.qualityReportModel
-                     .getProperty(resource, property)
-                     .getObject().toString();
-        } else {
-            StmtIterator iter = LoadQualityReportModel.qualityReportModel.listStatements();
-            while(iter.hasNext()){
-                Statement s = iter.nextStatement();
-                if (s.getSubject().getURI().toString().contains(resource.getURI().toString().replace("Problem", "Recommendation")) &&
-                    s.getPredicate().getURI().toString().contains(property.getURI().toString())
-                    )
-                {
-                    return s.getObject().toString();
-                }
-                    
-            }
-        }
-        return "";
+  private static final Logger LOG = Logger.getLogger(LoadQualityReportModel.class);
+
+  private static Model qualityReportModel = null;
+
+  public static String getResourcePropertyValue(Resource resource, Property property) {
+    if (resource == null || property == null) {
+      return "";
     }
-        
+
+    if (LoadQualityReportModel.qualityReportModel == null) {
+      LoadQualityReportModel.qualityReportModel = ModelFactory.createDefaultModel();
+
+      assert new File(Constants.QPROB_FILE).exists();
+
+      qualityReportModel = FileManager.get().loadModel(Constants.QPROB_FILE);
+      LOG.info("QPROB data model is loaded.");
+      // LoadQualityReportModel.qualityReportModel.read(filePathName);
+
+      // /* -- For Debug Only
+      // StmtIterator iter1 =
+      // LoadQualityReportModel.qualityReportModel.listStatements();
+      // while(iter1.hasNext()){
+      // Statement s = iter1.nextStatement();
+      // System.out.print("Subject :: " + s.getSubject());
+      // System.out.print("  --  Predicate :: " + s.getPredicate());
+      // System.out.println("  --> Object :: " + s.getObject());
+      // LOG.info("Subject :: " + s.getSubject() + "  --  Predicate :: " +
+      // s.getPredicate() +"  --> Object :: " + s.getObject() );
+      //
+      // }
+    }
+    if (LoadQualityReportModel.qualityReportModel.getProperty(resource, property) != null) {
+      return LoadQualityReportModel.qualityReportModel.getProperty(resource, property).getObject()
+          .toString();
+    } else {
+      StmtIterator iter = LoadQualityReportModel.qualityReportModel.listStatements();
+      while (iter.hasNext()) {
+        Statement s = iter.nextStatement();
+        if (s.getSubject().getURI().toString()
+            .contains(resource.getURI().toString().replace("Problem", "Recommendation"))
+            && s.getPredicate().getURI().toString().contains(property.getURI().toString())) {
+          return s.getObject().toString();
+        }
+
+      }
+    }
+    return "";
+  }
+
 }
