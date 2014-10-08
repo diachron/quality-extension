5cfe561cf0b334a52fb12edf16737b0054a44835
nfriesen <natalja.friesen@iais-extern.fraunhofer.d>
2014-10-08 18:07:30 +0200

    package containing classes required by web serveces is created

---
+++ src/com/google/refine/quality/webservices/MetricProcessing.java
@@ -0,0 +1,18 @@
+package com.google.refine.quality.webservices;
+
+import java.net.URI;
+import java.util.Set;
+
+public class MetricProcessing {
+	
+	
+	public void identifyQualityProblems(Set<String> metrics, URI filePath){
+		//create metric instances
+		//read file in a Stream
+		// compute metrics
+		//collectQualityProblems
+		//create QualityReport
+		
+	}
+
+}
---
+++ src/com/google/refine/quality/webservices/QualityReport.java
@@ -0,0 +1,14 @@
+package com.google.refine.quality.webservices;
+
+import java.net.URL;
+import java.util.List;
+
+import com.google.refine.quality.problems.QualityProblem;
+
+public class QualityReport {
+	
+	public void createQualityReport (URL outputPath, List<QualityProblem>problems){
+		
+	}
+
+}
