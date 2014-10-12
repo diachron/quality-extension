e6af35e0afbbcec4526bf891406233caf79448ca
Ruslan Kovalov <ruslan.kovalyov@gmail.com>
2014-10-12 21:26:17 +0200

    Edit ontology hijacking metrics, tests.

--- src/com/google/refine/quality/metrics/OntologyHijacking.java
+++ src/com/google/refine/quality/metrics/OntologyHijacking.java
@@ -1,12 +1,12 @@
 package com.google.refine.quality.metrics;
 
-
 import org.apache.log4j.Logger;
 import org.apache.xerces.util.URI;
 import org.apache.xerces.util.URI.MalformedURIException;
 
 import com.google.refine.quality.problems.QualityProblem;
 import com.google.refine.quality.utilities.VocabularyReader;
+import com.google.refine.quality.vocabularies.QPROB;
 
 import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.rdf.model.Model;
@@ -15,120 +15,98 @@ import com.hp.hpl.jena.sparql.core.Quad;
 import com.hp.hpl.jena.vocabulary.RDF;
 
 /**
- *  * TODO refactor
- * The Ontology Hijacking detects the redefinition by analyzing defined classes or 
- * properties in data set and looks of same definition in its respective vocabulary. 
+ * TODO
+ * The Ontology Hijacking detects the redefinition by analyzing defined classes
+ * or properties in data set and looks of same definition in its respective
+ * vocabulary.
  * 
- * Note: This class uses utilities.VocabularyReader to download models (vocabularies) from
- * web. Though VocabularyReader has it own cache but it has some inherit performance/scalability issues.   
- *  
- * @author Muhammad Ali Qasmi
- * @date 10th June 2014
- * 
+ * Note: This class uses utilities.VocabularyReader to download models
+ * (vocabularies) from web. Though VocabularyReader has it own cache but it has
+ * some inherit performance/scalability issues.
  */
 public class OntologyHijacking extends AbstractQualityMetric {
-        /**
-         * Description of quality report 
-         */
-        protected Resource qualityReport  = null;
-        /**
-         * logger static object
-         */
-        static Logger logger = Logger.getLogger(OntologyHijacking.class);
-        /**
-         * total number of locally defined classes or properties count 
-         */
-        protected double totalLocallyDefinedClassesOrPropertiesCount = 0;
-        /**
-         * total number of hijacked classes or properties found 
-         */
-        @Override
-        public void before(Object... args) {}
-        
-        @Override
-        public void after() {}
-        protected double hijackedClassesOrPropertiesCount = 0;
-        /**
-         * Check if the given quad has predicate of URI with given fragment
-         * 
-         * @param predicate
-         * @param fragment
-         * @return true if predicate is URI with given fragment
-         */
-        protected boolean isDefinedClassOrProperty(Quad quad, String fragment){
-                try {
-                       if (quad.getPredicate().isURI()) { // predicate is a URI
-                           URI tmpURI = new URI(quad.getPredicate().getURI());
-                           return (tmpURI.getFragment() != null && tmpURI.getFragment().toLowerCase().equals(fragment)) ?  true : false;
-                       }
-                       
-                } catch (MalformedURIException e) {
-                        logger.debug(e.getStackTrace());
-                        logger.error(e.getMessage());
-                }
-                return false;
-        }
-        
-        /**
-         * Detects if given node is defined in vocabulary or not
-         * 
-         * @param node
-         * @return true - if given node is found in the vocabulary with property of RDF.type
-         */
-        protected boolean isHijacked(Node node){
-                Model model = VocabularyReader.read(node.getURI());
-                if (model != null){
-                        if (model.getResource(node.getURI()).isURIResource()){
-                                if ( model.getResource(node.getURI()).hasProperty(RDF.type)) {
-                                     return true;
-                                }
-                        }
-                }
-                return false;
-        }
-        
-        /**
-         * Filters quad triples that are locally defined in the data set.
-         * Detects if filtered triples are already defined in vocabulary
-         */
-        @Override
-        public void compute(Quad quad) {
-                try {
-                    if (isDefinedClassOrProperty(quad, "type")){ // quad represent a locally defined statement
-                            this.totalLocallyDefinedClassesOrPropertiesCount++; // increments defined class or property count
-                            Node subject = quad.getSubject(); // retrieve subject
-                            if (isHijacked(subject)){ 
-                                    this.hijackedClassesOrPropertiesCount++; // increments redefined class or property count
-                                    QualityProblem reportProblems = new QualityProblem(quad, qualityReport);
-                                    problems.add(reportProblems);
-                            }
-                    }
-                    else if (isDefinedClassOrProperty(quad, "domain")){ // quad represent a locally defined statement
-                            this.totalLocallyDefinedClassesOrPropertiesCount++; // increments defined class or property count
-                            Node object = quad.getObject(); // retrieve object
-                            if (isHijacked(object)){ 
-                                    this.hijackedClassesOrPropertiesCount++; // increments redefined class or property count
-                                    QualityProblem reportProblems = new QualityProblem(quad, qualityReport);
-                                    problems.add(reportProblems);
-                            }
-                    }
-                }
-                catch (Exception e) {
-                     logger.debug(e.getStackTrace());
-                     logger.error(e.getMessage());
-                }
-        }
-        
-        /**
-         * Returns metric value for between 0 to 1. Where 0 as the best case and 1 as worst case 
-         * @return double - range [0 - 1] 
-         */
-        @Override
-        public double metricValue() {
-                if (this.totalLocallyDefinedClassesOrPropertiesCount <= 0) {
-                        logger.warn("Total classes or properties count is ZERO");
-                        return 0;
-                }
-                return (this.hijackedClassesOrPropertiesCount / this.totalLocallyDefinedClassesOrPropertiesCount);
-        }
+  private static final Logger LOG = Logger.getLogger(OntologyHijacking.class);
+  private Resource qualityReport = QPROB.OntologyHijackingProblem;
+
+  private long propertiesAndClasses = 0;
+  private long hijacked = 0;
+
+  @Override
+  public void before(Object... args) {};
+
+  @Override
+  public void after() {};
+
+  /**
+   * Check if the given quad has predicate of URI with given fragment
+   * 
+   * @param predicate
+   * @param fragment
+   * @return true if predicate is URI with given fragment
+   */
+  protected boolean isDefinedClassOrProperty(Quad quad, String fragment) {
+    try {
+      if (quad.getPredicate().isURI()) {
+        URI tmpURI = new URI(quad.getPredicate().getURI());
+        return (tmpURI.getFragment() != null &&
+            tmpURI.getFragment().toLowerCase().equals(fragment)) ? true : false;
+      }
+    } catch (MalformedURIException e) {
+      LOG.error(e.getMessage());
+    }
+    return false;
+  }
+
+  /**
+   * Detects if given node is defined in vocabulary or not
+   * @param node
+   * @return true - if given node is found in the vocabulary with property of RDF.type
+   */
+  protected boolean isHijacked(Node node) {
+    Model model = VocabularyReader.read(node.getURI());
+    if (model != null && model.getResource(node.getURI()).isURIResource()) {
+      if (model.getResource(node.getURI()).hasProperty(RDF.type)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * 
+   */
+  @Override
+  public void compute(Quad quad) {
+    if (isDefinedClassOrProperty(quad, "type")) {
+      propertiesAndClasses++;
+      Node subject = quad.getSubject();
+
+      if (isHijacked(subject)) {
+        hijacked++;
+        problems.add(new QualityProblem(quad, qualityReport));
+      }
+    } else if (isDefinedClassOrProperty(quad, "domain")) {
+      propertiesAndClasses++;
+      Node object = quad.getObject();
+
+      if (isHijacked(object)) {
+        hijacked++;
+        problems.add(new QualityProblem(quad, qualityReport));
+      }
+    }
+  }
+
+  /**
+   * Returns metric value for between 0 to 1. Where 0 as the best case and 1 as
+   * worst case
+   * @return double - range [0 - 1]
+   */
+  @Override
+  public double metricValue() {
+    if (propertiesAndClasses == 0) {
+      LOG.warn("Total classes or properties count is ZERO");
+      return 0;
+    }
+    return (double) hijacked / (double) propertiesAndClasses;
+  }
 }
---
+++ test/com/google/refine/quality/metrics/OntologyHijackingTest.java
@@ -0,0 +1,85 @@
+package com.google.refine.quality.metrics;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.sparql.core.Quad;
+import com.hp.hpl.jena.sparql.vocabulary.FOAF;
+import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.vocabulary.RDFS;
+
+import com.google.refine.quality.problems.QualityProblem;
+import com.google.refine.quality.utilities.Constants;
+
+public class OntologyHijackingTest {
+
+    private static AbstractQualityMetric metric;
+    private static List<Quad> quads = new ArrayList<Quad>();
+    private static Class<?> cls;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws ClassNotFoundException {
+      cls = Class.forName(String.format("%s.%s", Constants.METRICS_PACKAGE, "OntologyHijacking"));
+
+      Model model = ModelFactory.createDefaultModel();
+      model.createResource("http://example.org/#obj2").addProperty(RDF.type, FOAF.Person);
+      model.createResource("http://example.org/#obj1").addProperty(RDF.type, FOAF.Person);
+      model.createResource("http://example.org/#obj3")
+          .addProperty(RDF.type, RDF.Property)
+          .addProperty(RDFS.domain, RDFS.Class);
+      
+//      Resource rdfResource4 = model.createResource(RDF.type)
+//          .addProperty(RDF.type, RDFS.Class)
+//          .addProperty(RDFS.subClassOf, FOAF.Person);
+
+
+      StmtIterator si = model.listStatements();
+      while (si.hasNext()) {
+        quads.add(new Quad(null, si.next().asTriple()));
+      }
+    }
+
+    @Test
+    public void emptyQuads() throws IllegalAccessException, IllegalArgumentException,
+        InvocationTargetException, NoSuchMethodException, SecurityException, InstantiationException {
+      metric = (AbstractQualityMetric) cls.newInstance();
+      metric.getClass().getDeclaredMethod("before", Object[].class)
+          .invoke(metric, new Object[] { new String[] {} });
+
+      metric.compute(new ArrayList<Quad>());
+      List<QualityProblem> problems = metric.getQualityProblems();
+      Assert.assertTrue(problems.isEmpty());
+      Assert.assertTrue(problems.size() == 0);
+      Assert.assertEquals(0.0, metric.metricValue(), 0.0);
+    }
+
+    @Test
+    public void metric() throws IllegalAccessException, IllegalArgumentException,
+        InvocationTargetException, NoSuchMethodException, SecurityException, InstantiationException {
+      metric = (AbstractQualityMetric) cls.newInstance();
+      metric.getClass().getDeclaredMethod("before", Object[].class)
+          .invoke(metric, new Object[] { new String[] {} });
+
+      metric.compute(quads);
+      List<QualityProblem> problems = metric.getQualityProblems();
+      Assert.assertFalse(problems.isEmpty());
+       Assert.assertTrue(problems.size() == 1);
+      Assert.assertEquals(0.25, metric.metricValue(), 0.0);
+    }
+
+    @AfterClass
+    public static void tearDown() throws IllegalAccessException, IllegalArgumentException,
+        InvocationTargetException, NoSuchMethodException, SecurityException {
+      metric.getClass().getMethod("after", (Class[]) null).invoke(metric, (Object[]) null);
+    }
+  }
